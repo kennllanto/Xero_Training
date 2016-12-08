4c5e384167e628bf0b709ff493ce5f5fff4caaed
kennllanto <kennllanto@gmail.com>
2016-12-08 18:03:07 +1100

    test

---
+++ TodoList/TodoList/TodoListClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    class TodoListClient
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TodoList/TodoListDataLayer/TodoListDataLayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListDataLayer
+{
+    public class TodoListDataLayer
+    {
+        private List<string> fileContents;
+        const string toDoListFileName = "todolist.txt";
+
+        public TodoListDataLayer()
+        {
+            string path = Directory.GetCurrentDirectory();
+            string[] lines = File.ReadAllLines(path + toDoListFileName);
+            fileContents =  lines.ToList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textToWrite"></param>
+        /// <returns></returns>
+        public bool StoreData(string textToWrite)
+        {
+            StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.UTF8);
+            streamWriter.WriteLine(textToWrite);
+            streamWriter.Flush();
+            streamWriter.Close();
+            fileStream.Close();
+
+            fileStream.Open
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ReadData()
+        {
+            StreamReader r = new StreamReader(fs, Encoding.UTF8);
+            Console.WriteLine(r.ReadLine());
+            return true;
+        }
+
+        public bool DeleteData()
+        {
+            return true;
+        }
+
+    }
+}
